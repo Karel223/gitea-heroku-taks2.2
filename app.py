21a87aa6c446bfb6106c6a21424a102667fd4236
Karel223 <karol.hadzicki@onet.pl>
2024-12-22 19:32:27 +0100

    Fix port configuration and add Procfile

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Heroku with Docker and Python!"
+    return "Hello, Heroku!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Pobierz PORT z ustawień Heroku
+    app.run(host="0.0.0.0", port=port)
